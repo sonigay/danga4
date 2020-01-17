b9fb85a06ea3566f933a4cb5e46de9f6b4429c6c
sonigay <43840048+sonigay@users.noreply.github.com>
2020-01-17 18:45:52 +0900

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import datetime
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-scope = ['https://spreadsheets.google.com/feeds']
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name('Quick-4d4e63a22b80.json', scope)
 client = gspread.authorize(creds)
 doc = client.open_by_url('https://docs.google.com/spreadsheets/d/1WzFr_aNi6T_ievrOaVJK3hT2tRQsjltfACC6WL2qXNI')
